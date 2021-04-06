3164a67720834735eb114a363189ff43474d1e03
Ivan Polonevich <ivan.polonevich@pandadoc.com>
2021-04-06 15:44:11 +0300

    Write each request to file; Continue work from previous run;

--- mailgun_events.py
+++ mailgun_events.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-import os
-import sys
-import requests
-import json
 
-if len(sys.argv) != 3:
+import json
+import requests
+import sys
+from os import path
+
+if len(sys.argv) != 4:
     print("Usage: mailgun_events.py 'api-key' 'example.com' 'rejected OR failed'")
     sys.exit(1)
 
 filename = "result.json"
+page_url = 'page_url.txt'
 
 api_key = sys.argv[1]
 domain = sys.argv[2]
@@ -16,11 +18,17 @@ filters = sys.argv[3]
 
 url = "https://api.mailgun.net/v3/%s/events"
 url = url % domain
-params = {"event": filters, "limit": 300}
+params = {"event": filters}
 
-items = []
+item = []
 last_next_url = None
 count = 0
+
+if path.exists(page_url) and path.getsize(page_url) > 0:
+    f = open(page_url, "r")
+    url = f.readline()
+    print(url)
+
 while url is not last_next_url:
     r = requests.get(
         url,
@@ -28,13 +36,28 @@ while url is not last_next_url:
         params=params
     )
 
-    items += r.json()["items"]
+    item += r.json()["items"]
     print(count)
+    print(url)
+
+    if item != 0:
+        if path.exists(filename) and path.getsize(filename) > 0:
+            with open(filename, "r+") as outfile:
+                    data = json.load(outfile)
+                    data.append(item)
+                    outfile.seek(0)
+                    json.dump(data, outfile)
+                    print('Update results')
+        else:
+            with open(filename, "w") as outfile:
+                json.dump(item, outfile)
+                print('Init results')
+
 
     if 200 == r.status_code and not len(r.json()["items"]) == 0:
         url = r.json()["paging"]['next']
+        f = open(page_url, "w")
+        f.write(url)
+        f.close()
     count += count + 1
 
-if items != 0:
-    with open(filename, "w") as outfile:
-        json.dump(items, outfile)
